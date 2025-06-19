331eb665f6471345545b65e606f7759de37247a0
L3x13n <l3x13n@gmail.com>
2025-06-19 22:08:58 +0100

    updated them

---
+++ wp-content/themes/hypermigratefull/single.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * single.php
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ * Template Name: Front page 2
+ */
+
+get_header();
+
+/* Start the Loop */
+get_template_part( 'template-parts/content/content-page' );
+
+
+get_footer();
